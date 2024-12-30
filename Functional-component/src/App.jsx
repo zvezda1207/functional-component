5ac5d23c1ed0fd75d2dd8e60607000f8f08e4b86
Ira Tka4eva <ira.tka4eva2011@yandex.ru>
2024-12-30 15:00:21 +0300

    Second commit

--- Functional-component/src/App.jsx
+++ Functional-component/src/App.jsx
@@ -2,7 +2,28 @@ import './App.css';
 import { ShopItemFunc } from './components/ShopItemFunc/ShopItemFunc'
 
 function App() {
-  return <ShopItemFunc />
+  const item = {
+    brand: 'Tiger of Sweden',
+    title: 'Leonard coat',
+    description: 'Minimalistic coat in cotton-blend',
+    descriptionFull: 'Men\'s minimalistic overcoat in cotton-blend. Features a stand-up collar, concealed front closure and single back vent. Slim fit with clean, straight shape. Above-knee length.',
+    price: 399,
+    currency: '£'
+  }
+  
+  // Внутри компонента App
+  return (
+    <div className="container">
+      <div className="background-element">
+      </div>
+      <div className="highlight-window">
+        <div className='highlight-overlay'></div>
+      </div>
+      <div className="window">
+        <ShopItemFunc item={item} />
+      </div>
+    </div>
+  )
 }
 
 export default App
--- Functional-component/src/components/ShopItemFunc/ShopItemFunc.jsx
+++ Functional-component/src/components/ShopItemFunc/ShopItemFunc.jsx
@@ -1,29 +1,24 @@
 import React from 'react';
 import './shopItemFunc.css'
 
-export const ShopItemFunc = () => {
-    const item = {
-        brand: 'Tiger of Sweden',
-        title: 'Leonard coat',
-        description: 'Minimalistic coat in cotton-blend',
-        descriptionFull: 'Men\'s minimalistic overcoat in cotton-blend. Features a stand-up collar, concealed front closure and single back vent. Slim fit with clean, straight shape. Above-knee length.',
-        price: 399,
-        currency: '£'
-      };
+export const ShopItemFunc = ({ item }) => {
   return (
-    <div class="main-content">
-      <h2>Tiger of Sweden</h2>
-      <h1>Leonard coat</h1>
-      <h3>Minimalistic coat in cotton-blend</h3>
-      <div class="description">
-        Men's minimalistic overcoat in cotton-blend. Features a stand-up collar, concealed front closure and single back vent. Slim fit with clean, straight shape. Above-knee length.
+    <div className="main-content">
+      <h2>{item.brand}</h2>
+      <h1>{item.title}</h1>
+      <h3>{item.description}</h3>
+      <div className="description">
+        {item.descriptionFull}
       </div>
-      <div class="highlight-window mobile"><div class="highlight-overlay"></div></div>
-      <div class="divider"></div>
-      <div class="purchase-info">
-      <div class="price">£399.00</div>
+      <div className="highlight-window mobile">
+        <div className="highlight-overlay">
+        </div>
+      </div>
+      <div className="divider"></div>
+      <div className="purchase-info">
+      <div className="price">{item.currency}{item.price}.00</div>
       <button>Добавить в корзину</button>
       </div>
     </div>
-  )
-}
+  );
+};
